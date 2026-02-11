1139195dc559f6d4369f0629a4694964e707d2d4
Natallia <uvarovn771@gmail.com>
2026-02-11 20:04:31 +0000

    пожалуйста это очень тяжело ставить билды постоянно - прошу проверь весь

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -53,14 +53,8 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
-      }}
+      iconLeft={<ChevronLeft className="h-4 w-4" />}
+      iconRight={<ChevronRight className="h-4 w-4" />}
       {...props}
     />
   )
