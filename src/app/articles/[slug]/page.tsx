d29069033d0617c68d4e5f5f3a009d3cbad68bb9
Natallia <uvarovn771@gmail.com>
2026-02-11 23:23:53 +0000

    теперь я вижу полностью удаленный код но так и не вижу фикса решеток в т

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -33,7 +33,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function ArticlePage({
+export default function ArticlePage({
   params,
 }: {
   params: { slug:string };
